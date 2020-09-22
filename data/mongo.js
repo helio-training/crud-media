18345420a775f67911d0ce138b491ec27fa92cb1
Wes Cutting <wesleycutting@gmail.com>
2020-09-22 11:08:52 -0600

    Added DAL for Video Games

---
+++ data/mongo.js
@@ -0,0 +1,72 @@
+const { MongoClient } = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
+
+let client;
+
+function connect(locals) {
+
+    const uri = process.env.DB_URL;
+
+    client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    return client.connect()
+        .then((connection) => {
+            locals.collectionVideoGames = connection.db('games').collection('video-games');
+        })
+        .catch(err => {
+            console.log(err);
+            process.exit();
+        })
+}
+
+function close() {
+    client.close();
+}
+
+function readAll(info) {
+
+    return info.collection.find(info.query).toArray();
+}
+
+function readOne(info) {
+
+    return info.collection.findOne(info.query);
+
+}
+
+function readOneById(info) {
+
+    return info.collection.findOne({ _id: ObjectID(info.id) });
+}
+
+function createOne(info) {
+
+    return info.collection.insertOne(info.doc);
+
+}
+
+function replaceOne(info) {
+
+    return info.collection.findOneAndReplace(info.query, info.doc);
+}
+
+function changeOne(info) {
+
+    return info.collection.findOneAndUpdate(info.query, { $set: info.doc });
+}
+
+function deleteOne(info) {
+
+    return info.collection.deleteOne(info.query);
+
+}
+
+module.exports.connect = connect;
+module.exports.close = close;
+module.exports.readAll = readAll;
+module.exports.readOne = readOne;
+module.exports.createOne = createOne;
+module.exports.readOneById = readOneById;
+module.exports.replaceOne = replaceOne;
+module.exports.changeOne = changeOne;
+module.exports.deleteOne = deleteOne;
